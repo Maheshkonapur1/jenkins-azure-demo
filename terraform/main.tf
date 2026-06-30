2d8bacd44df91cbf6952272f183c26c3a4c06594
Maheshkonapur1 <mahesh_konapur@thbs.com>
2026-06-30 18:27:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~>4.0"
     }
   }
 }
@@ -12,7 +12,34 @@ provider "azurerm" {
   subscription_id = "fc144c45-622c-4cfc-9a12-d4cab04b295b"
 }
 
+variable "resource_group_name" {
+  default = "mahesh-rg"
+}
+
+variable "location" {
+  default = "Central India"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "mahesh-app-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+
+  name                = "mahesh-webapp-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config { }
 }
