19179843c3bc83d06e9775dba9c211d4458af45d
Maheshkonapur1 <mahesh_konapur@thbs.com>
2026-06-30 18:15:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,11 @@ provider "azurerm" {
   subscription_id = "fc144c45-622c-4cfc-9a12-d4cab04b295b"
 }
 
+variable "resource_group_name" {
+  type = string
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "mahesh-rg"
+  name     = var.resource_group_name
   location = "Central India"
 }
